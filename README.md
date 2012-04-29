e721fcf102a1760015a62b7cf3292bba35704355
Bill Chapman <wwchapman@gmail.com>
2012-04-28 23:56:43 -0400

    Added root fallback for revert task

--- README.md
+++ README.md
@@ -12,15 +12,13 @@ The basic idea is that anyone on the team who already has key based access as a 
 to others. You or your system administrator may disagree but in practice this makes sense for small to medium sized teams.
 
 The idea is pretty simple:
-  Keep a local list of users, servers and keys. Any team member with key based access to a server can make changes to that server.
-I recommend not using this for root accounts. 
+  Keep a local list of users and public keys that can be sync'd to many hosts that contain those users.
+
+I recommend not using this for root accounts. Basically always make sure there is an account that you can access to revert any
+rogue changes that render an account non accessible. 
+ 
 • Connect to each host in the host list for a given user
 • Write out the new authorized_keys.tmp file based on the locally enabled keys
 • Test the new key file
 • If any errors were encountered the old key file remains in place
-• Otherwise the new key file remains in place
-
-#Dependencies
-
-• Capistrano
 
--- lib/moranis/key_master.rb
+++ lib/moranis/key_master.rb
@@ -1,6 +1,12 @@
 module Moranis
   class KeyMaster
-  
+    
+    #TODO: move this in to a configuration object
+    #this file is either authorized_keys, authorized_keys2 or authorized depending on ssh version
+    #I only needed ssh1 support for my situation but this is here for convenience and can be changed 
+    #by setting Moranis::Keymaster::KEY_FILE = 
+    KEY_FILE = "authorized_keys"
+    
     def initialize(config_path)
       @config = YAML::load(File.open(config_path))
     end
@@ -33,6 +39,7 @@ module Moranis
             remote_run.commands << "echo \"#{key}\" >> ~/.ssh/authorized_keys.tmp"
           end
           
+          #swap the key files
           remote_run.commands << "mv ~/.ssh/authorized_keys ~/.ssh/authorized_keys.bak"
           remote_run.commands << "mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys"
           
@@ -49,8 +56,17 @@ module Moranis
       hosts.each do |host|
         users.each do |user|
           remote_run = Moranis::RemoteRun.new(host,user)
-          remote_run.commands << "if [ -f ~/.ssh/authorized_keys.bak  ] then mv ~/.ssh/authorized_keys.bak ~/.ssh/authorized_keys fi"
-          remote_run.execute
+          begin
+            remote_run.commands << "if [ -f ~/.ssh/authorized_keys.bak  ] then mv ~/.ssh/authorized_keys.bak ~/.ssh/authorized_keys fi"
+            remote_run.execute
+          rescue
+            #we tried as the user, the keys file may be messed up or our key does not exist in the list any longer
+            # lets try as root
+            root_remote_run = Moranis::RemoteRun.new(host,"root")
+            root_remote_run = "su - #{user}"
+            root_remote_run.commands << "if [ -f ~/.ssh/authorized_keys.bak  ] then mv ~/.ssh/authorized_keys.bak ~/.ssh/authorized_keys fi"
+            root_remote_run.execute rescue "You may not have root priviledge for reversion"
+          end
         end
       end
       
